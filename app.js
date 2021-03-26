78b9f92cfe8141167aac90b976dbd0be97f2201c
hannahdh98 <56090227+hannahdh98@users.noreply.github.com>
2021-03-25 21:02:43 -0400

    updated files for week 6 and 7

---
+++ app.js
@@ -0,0 +1,51 @@
+//import path
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//imports error.js
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+//imports modules
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+//has the bodyParser, pardt the body that is sent throhght the form
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use ((req, res, next) =>
+//    User.findbyId()
+//    .then
+// });
+app.use((req, res, next) => {
+  //uses id to search for that user
+  User.findById('6048fe5037c6e1ff075b8ed1')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
+//router object
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+//middleware to handle 404
+//app.use((req, res, next) => {
+      // res.status(404).render('404', {pageTitle: 'Page Not Found });
+//});
+
+//get404 function
+app.use(errorController.get404);
+
+mongoConnect(() => {
+  app.listen(3000);
+});
---
+++ controllers/admin.js
@@ -0,0 +1,108 @@
+//const mongodb = require ('mongodb'); 
+const Product = require('../models/product');
+
+//const ObjectId - mongodb.ObjectId;
+
+//export function
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
+};
+
+//pushes through new products through the post route
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  
+  //creates new product based of the class
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
+  product
+    .save()
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    // Product.findByPk(prodId)
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log(err));
+};
+//gets new info for the product
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
+    .then(result => {
+      console.log('UPDATED PRODUCT!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
+//delete product with postDeleteProduct
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId)
+    .then(() => {
+      console.log('DESTROYED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+};
